d6a9c41d8235614462d0ab402cdce312a1d56035
Robyn H <robynhotta@gmail.com>
2019-04-03 20:22:04 +0000

    add function into if statement

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
+
+
 console.log("HIOIIIOOO");
 var letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'W'];
 var randomLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
 var letterbtn = document.getElementById("letterbtn");
 var letterDisplay = document.getElementById("letterDisplay");
 
-letterbtn.addEventListener("click", function(){
+console.log(letterDisplay);
+if(letterbtn){
+    letterbtn.addEventListener("click", function(){
 	letterDisplay.textContent = randomLetter;
-});
+})}
