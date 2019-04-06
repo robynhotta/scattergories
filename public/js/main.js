595c864690b8545b3d06c5877ca53c3627e0e27d
Robyn H <robynhotta@gmail.com>
2019-04-06 00:04:01 +0000

    add reset function

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,24 @@
-
-
 console.log("HIOIIIOOO");
 var letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'W'];
 var randomLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
 var letterbtn = document.getElementById("letterbtn");
 var letterDisplay = document.getElementById("letterDisplay");
 var resetbtn = document.getElementById("resetbtn");
-var form = document.querySelector("form-group")
+var form = document.querySelectorAll("div.form-group .input");
 
 if(letterbtn){
     letterbtn.addEventListener("click", updateLetter);
 }
 
 function updateLetter() {
-    console.log(randomLetter);
     letterDisplay.textContent = randomLetter;
 }
 
 if(resetbtn){
     resetbtn.addEventListener("click", reset);
-}
+};
 
 function reset(){
-    form.textContent = " ";
+    console.log(form)
+    form.textContent = "";
 }
