76c5ab851d0ccde26f6f9b562496ff418df8cd4b
Robyn H <robynhotta@gmail.com>
2019-04-10 16:15:15 +0000

    add reset function not fully functional

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,28 @@ console.log("HIOIIIOOO");
 var letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'W'];
 var randomLetter = letterArray[Math.floor(Math.random() * letterArray.length)];
 var letterbtn = document.getElementById("letterbtn");
-var letterDisplay = document.getElementById("letterDisplay");
 var resetbtn = document.getElementById("resetbtn");
-var form = document.querySelectorAll("div.form-group .input");
+var letterDisplay = document.getElementById("letterDisplay");
+var form = document.getElementsByClassName("box");
 
 if(letterbtn){
     letterbtn.addEventListener("click", updateLetter);
 }
 
 function updateLetter() {
-    letterDisplay.textContent = randomLetter;
+    letterArray.forEach(function(){
+        letterDisplay.textContent = randomLetter;
+    });
 }
 
 if(resetbtn){
     resetbtn.addEventListener("click", reset);
-};
+}
 
-function reset(){
-    console.log(form)
-    form.textContent = "";
+function reset() {
+    letterDisplay.textContent = randomLetter;
+    for(var i = 0; i < form.length; i++){
+        form[i].value = "";
+    }
+
 }
